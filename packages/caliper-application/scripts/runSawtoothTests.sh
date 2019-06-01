686a2a0ddfa04378e15c9f882f90cbbed031495b
arni.woznica <arni.woznica@gmail.com>
2019-06-01 18:54:16 +0200

    changed waiting time in sawtooth test cases, changed fabric test to single node mode, added scripts to run tests for a network automatically

---
+++ packages/caliper-application/scripts/runSawtoothTests.sh
@@ -0,0 +1,7 @@
+node run-benchmark.js -c ../benchmark/simple/sawtoothTestShort.yaml -n ../network/sawtooth-poet/sawtooth-poet-5-validators/sawtooth.json 
+node run-benchmark.js -c ../benchmark/simple/sawtoothTestShort.yaml -n ../network/sawtooth-poet/sawtooth-poet-10-validators/sawtooth.json 
+node run-benchmark.js -c ../benchmark/simple/sawtoothTestShort.yaml -n ../network/sawtooth-poet/sawtooth-poet-20-validators/sawtooth.json 
+node run-benchmark.js -c ../benchmark/simple/sawtoothTestShort.yaml -n ../network/sawtooth-poet/sawtooth-poet-30-validators/sawtooth.json 
+node run-benchmark.js -c ../benchmark/simple/sawtoothTestShort.yaml -n ../network/sawtooth-poet/sawtooth-poet-40-validators/sawtooth.json 
+node run-benchmark.js -c ../benchmark/simple/sawtoothTestShort.yaml -n ../network/sawtooth-poet/sawtooth-poet-50-validators/sawtooth.json 
+node run-benchmark.js -c ../benchmark/simple/sawtoothTestShort.yaml -n ../network/sawtooth-poet/sawtooth-poet-60-validators/sawtooth.json 
---
+++ packages/caliper-application/scripts/runfabricTests.sh
@@ -0,0 +1,11 @@
+node run-benchmark.js -c ../benchmark/simple/fabricTest.yaml -n ../network/fabric-v1.4/1org5peer/fabric-go.json 
+node run-benchmark.js -c ../benchmark/simple/fabricTest.yaml -n ../network/fabric-v1.4/1org10peer/fabric-go.json 
+node run-benchmark.js -c ../benchmark/simple/fabricTest.yaml -n ../network/fabric-v1.4/1org20peer/fabric-go.json 
+node run-benchmark.js -c ../benchmark/simple/fabricTest.yaml -n ../network/fabric-v1.4/1org30peer/fabric-go.json 
+node run-benchmark.js -c ../benchmark/simple/fabricTest.yaml -n ../network/fabric-v1.4/1org40peer/fabric-go.json 
+node run-benchmark.js -c ../benchmark/simple/fabricTest.yaml -n ../network/fabric-v1.4/1org50peer/fabric-go.json 
+node run-benchmark.js -c ../benchmark/simple/fabricTest.yaml -n ../network/fabric-v1.4/1org60peer/fabric-go.json 
+node run-benchmark.js -c ../benchmark/simple/fabricTest.yaml -n ../network/fabric-v1.4/1org70peer/fabric-go.json 
+node run-benchmark.js -c ../benchmark/simple/fabricTest.yaml -n ../network/fabric-v1.4/1org80peer/fabric-go.json 
+node run-benchmark.js -c ../benchmark/simple/fabricTest.yaml -n ../network/fabric-v1.4/1org90peer/fabric-go.json 
+node run-benchmark.js -c ../benchmark/simple/fabricTest.yaml -n ../network/fabric-v1.4/1org100peer/fabric-go.json 
